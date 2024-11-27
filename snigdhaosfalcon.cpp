187ccdbb3216c0839eadd4807ac5c85b3be3ea9b
Eshan Roy <m.eshanized@gmail.com>
2024-11-27 13:45:17 +0530

    🛠️ build(_ui): update + upgrade graphic

--- snigdhaosfalcon.cpp
+++ snigdhaosfalcon.cpp
@@ -8,6 +8,7 @@ SnigdhaOSFalcon::SnigdhaOSFalcon(QWidget *parent)
 {
     ui->setupUi(this);
     updateCheckBoxes();
+    connect(&installer, SIGNAL(finished(int,QProcess::ExitStatus)), this, SLOT(on_installerFinished(int,QProcess::ExitStatus)));
 }
 
 SnigdhaOSFalcon::~SnigdhaOSFalcon()
@@ -15,6 +16,23 @@ SnigdhaOSFalcon::~SnigdhaOSFalcon()
     delete ui;
 }
 
-void SnigdhaOSFalcon::updatePackageList() {
+QList<QCheckBox*> SnigdhaOSFalcon::getCheckboxes() {
+    return this->ui->tabWidget->findChildren<QCheckBox*>();
+}
 
+void SnigdhaOSFalcon::updatePackageList() {
+    QStringList packages;
+    for (auto element : getCheckboxes()) {
+        auto string = element->property("packageName").toString();
+        if (string.isEmpty()) {
+            std::runtime_error(("Empty/Non existent package name detected for " + element->objectName()).toStdString());
+        }
+        packages.append(std::move(string));
+    }
+
+    QProcess proc;
+    proc.start("pacman -Qq " + packages.join(" "));
+    proc.waitForFinished();
+    QString installed = proc.readAllStandardOutput();
+    installed_packages = installed.split("\n");
 }
